4776753f7ef944ec96175d8128594115f21b629b
fivesmallq <fivesmallq@gmail.com>
2014-07-09 18:58:57 +0800

    set license

--- src/main/java/org/nll/hbase/ui/App.java
+++ src/main/java/org/nll/hbase/ui/App.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright 2014 fivesmallq.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.nll.hbase.ui;
 
-import org.nll.hbase.ui.component.StartFrame;
 import java.awt.Font;
 import java.util.Enumeration;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.plaf.FontUIResource;
+import org.nll.hbase.ui.component.StartFrame;
 
 public class App {
 
